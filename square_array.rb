745b3f12d19592840066bb059c6d74de63946f31
Vytautas Klimavicius <vyklimavicius@gmail.com>
2019-01-31 16:44:10 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
-  new_array = []
-  array.each |num| do
-  new_array << num * num 
+  new_array = Array.new
+  array.each do |num|
+    new_array << num * num 
 end 
-  new_array 
+  new_array
 end
